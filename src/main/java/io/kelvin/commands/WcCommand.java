75715b9272833045c6c2a3eeec0b0b459d873e90
Kelvin Dervishllari <kelvin.dervishllari@proton.me>
2024-02-14 19:19:43 +0100

    print new lines count only functionality

--- src/main/java/io/kelvin/commands/WcCommand.java
+++ src/main/java/io/kelvin/commands/WcCommand.java
@@ -5,9 +5,12 @@ import picocli.CommandLine.Option;
 import picocli.CommandLine.Parameters;
 
 import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.util.stream.Stream;
 
 @Command(
-        name = "ccwc",
+        name = "pro-wc",
         mixinStandardHelpOptions = true,
         version = "Word count 1.0",
         description = "Print  newline,  word,  and  byte counts for each FILE, and a total line if more than one\n" +
@@ -19,12 +22,38 @@ public class WcCommand implements Runnable {
     File file;
 
     @Option(names = {"-c", "--bytes"}, description = "print the byte counts")
-    boolean printBytesOnly;
+    boolean printBytesCountOnly;
+
+    @Option(names = {"-l", "--print the newline counts"}, description = "print the newline counts")
+    boolean printNewLinesCountOnly;
 
     @Override
     public void run() {
-        if (printBytesOnly){
-            System.out.println(file.length() + " " + file.getName());
+        if (printBytesCountOnly){
+            System.out.println(this.file.length() + " " + this.file.getName());
+        }
+        if (printNewLinesCountOnly) {
+            printNewLinesCountOnly(this.file);
+        }
+    }
+
+    private void printNewLinesCountOnly(File file) {
+        if (!file.exists()){
+            System.out.println("File doesn't exists!");
+            return;
+        }
+        if (!file.isFile()){
+            System.out.println("Path does not point to a file!");
+            return;
+        }
+        if (!file.canRead()){
+            System.out.println("File is not readable!");
+            return;
+        }
+        try(Stream<String> lines = Files.lines(file.toPath())) {
+            System.out.println(lines.count() + " " + file.getName());
+        } catch (IOException e) {
+            System.out.println("Error occurred when trying to read lines of " + file.getName());
         }
     }
 }
